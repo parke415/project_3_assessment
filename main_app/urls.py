ee27a41c1ced4315d9271dc030881e3623aaeab0
parke415 <parke415@gmail.com>
2021-01-08 12:58:07 -0800

    Complete Step 3.3

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('add_widget/', views.add_widget, name='add_widget'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Widget
+from .forms import WidgetForm
 
 def index(request):
   widget_list = Widget.objects.all()
-  return render(request, 'index.html', {'widget_list': widget_list})
+  widget_form = WidgetForm()
+  return render(request, 'index.html', {'widget_list': widget_list, 'widget_form': widget_form})
+
+def add_widget(request):
+  form = WidgetForm(request.POST)
+  if form.is_valid():
+    new_widget = form.save(commit=False)
+    new_widget.save()
+  return redirect('/')
